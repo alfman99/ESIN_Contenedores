57be2d5c6fa7925a9470d5bfafd91d77b7517230
Alfredo <amanresamartinez@hotmail.com>
2020-11-23 17:13:15 +0100

    initial commit

---
+++ contenidor.cpp
@@ -0,0 +1,117 @@
+#include "contenidor.hpp"
+#include "contenidor.rep"
+
+/* Constructora. Crea un contenidor amb matrícula m i longitud l.
+    Es produeix un error si m no és una seqüència de un o més caràcters,
+    formada exclusivament per lletres majúscules i dígits,
+    o si l no pertany a {10, 20, 30} */
+contenidor::contenidor(const string &m, nat l) throw(error){
+    matri = m;
+    longi = l;
+    if (this->es_error(m, l)) {
+        throw error(MatriculaIncorrecta);
+    }
+}
+
+/* Constructora per còpia, assignació i destructora. */
+contenidor::contenidor(const contenidor &u) throw(error){
+    matri = u.matri;
+    longi = u.longi;
+}
+
+contenidor& contenidor::operator=(const contenidor &u) throw(error){
+    if(this != &u){
+        matri = u.matri;
+        longi = u.longi;
+    }
+    return *this;
+}
+
+contenidor::~contenidor() throw() {}
+
+/* Consultors. Retornen respectivament la longitud i la matrícula del
+    contenidor. */
+nat contenidor::longitud() const throw(){
+    return longi;
+}
+
+string contenidor::matricula() const throw(){
+    return matri;
+}
+
+/* Operadors de comparació. L'operador d'igualtat retorna cert si i
+    només si els dos contenidors contenen la mateixa matrícula i longitud.
+    L'operador menor retorna cert si i només si la matrícula del
+    paràmetre implícit es més petit en ordre alfabètic que la c o si les
+    dues matrícules són iguals i la longitud del paràmetre implícit és més
+    petita que la de c. La resta d'operadors es defineixen consistentment
+    respecte a <. */
+
+bool contenidor::operator==(const contenidor &c) const throw(){
+    return matri == c.matri && longi == c.longi;
+}
+
+bool contenidor::operator!=(const contenidor &c) const throw(){
+    return matri != c.matri || longi != c.longi;
+}
+
+bool contenidor::operator<(const contenidor &c) const throw(){
+    if (this->matri < c.matri) {
+        return true;
+    } 
+    else if (this->matri == c.matri) {
+        if (this->longi < c.longi) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
+}
+
+bool contenidor::operator<=(const contenidor &c) const throw(){
+    return *this < c || *this == c;
+}
+
+bool contenidor::operator>(const contenidor &c) const throw(){
+    if (this->matri > c.matri) {
+        return true;
+    } 
+    else if (this->matri == c.matri) {
+        if (this->longi > c.longi) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
+}
+
+bool contenidor::operator>=(const contenidor &c) const throw(){
+    return *this > c || *this == c;
+}
+
+bool es_error(const string &m, nat l) {
+    if (l != 10 || l != 20 || l != 30) {
+        return true;
+    }
+    return checkMatricula(m);
+}
+
+bool checkMatricula (const string &matricula) {
+    if (matricula.size() <= 0) {
+        return true;
+    }
+    for(char letra : matricula) {
+        if (!((48 <= letra && letra <= 57) || (65 <= letra && letra <= 90) || (97 <= letra && letra <= 122))) {
+            return true;
+        }
+    }
+    return false;
+}
---
+++ ubicacio.cpp
@@ -0,0 +1,104 @@
+#include "ubicacio.hpp"
+#include "ubicacio.rep"
+
+ubicacio::ubicacio(int i, int j, int k) throw(error) {
+    this->fila = i;
+    this->plaza = j;
+    this->piso = k;
+    if (es_error(i, j, k)) {
+        throw error(UbicacioIncorrecta);
+    }
+}
+
+/* Constructora per còpia, assignació i destructora. */
+ubicacio::ubicacio(const ubicacio& u) throw(error) {
+    fila = u.fila;
+    plaza = u.plaza;
+    piso = u.piso;
+}
+
+ubicacio& ubicacio::operator=(const ubicacio& u) throw(error) {
+    if (this != &u) {
+        this->fila = u.fila;
+        this->plaza = u.plaza;
+        this->piso = u.piso;
+    }
+    return *this;
+}
+
+ubicacio::~ubicacio() throw() {}
+
+/* Consultors. Retornen respectivament el primer, segon i tercer
+   component de la ubicació. */
+int ubicacio::filera() const throw() {
+    return fila;
+}
+
+int ubicacio::placa() const throw() {
+    return plaza;
+}
+
+int ubicacio::pis() const throw() {
+    return piso;
+}
+
+/* Operadors de comparació.
+   L'operador d'igualtat retorna cert si i només si les dues ubicacions
+   tenen la mateixa filera, plaça i pis. L'operador menor retorna cert si
+   i només si la filera del paràmetre implícit és més petit que la
+   d'u, o si les dues fileres són iguals i la plaça del paràmetre
+   implícit és més petita que la d'u, o si les fileres i les places
+   coincideixen i el pis del paràmetre implícit és més petit que el d'u.
+   La resta d'operadors es defineixen consistentment respecte <. */
+bool ubicacio::operator == (const ubicacio & u) const throw() {
+    return fila == u.fila && plaza == u.plaza && piso == u.piso;
+}
+
+bool ubicacio::operator != (const ubicacio & u) const throw() {
+    return fila != u.fila || plaza != u.plaza || piso != u.piso;
+}
+
+bool ubicacio::operator < (const ubicacio & u) const throw() {
+    if (this->fila < u.fila) return true;
+    else if (this->fila == u.fila) {
+        if (this->plaza < u.plaza) return true;
+        else if (this->plaza == u.plaza) {
+            if (piso < u.piso) return true;
+            else return false;
+        }
+        else return false;
+    }
+    else return false;
+}
+
+bool ubicacio::operator <= (const ubicacio & u) const throw() {
+    return *this < u || *this == u;
+}
+
+bool ubicacio::operator > (const ubicacio & u) const throw() {
+    if (this->fila > u.fila) return true;
+    else if (this->fila == u.fila) {
+        if (this->plaza > u.plaza) return true;
+        else if (this->plaza == u.plaza) {
+            if (this->piso > u.piso) return true;
+            else return false;
+        }
+        else return false;
+    }
+    else return false;
+}
+
+bool ubicacio::operator >= (const ubicacio & u) const throw() {
+    return *this > u || *this == u;
+}
+
+bool ubicacio::es_error(int i, int j, int k) {
+    bool b;
+    if ((i >= 0 && j >= 0 && k >= 0) || (i == -1 && j == 0 && k == 0) || (i == -1 && j == -1 && k == -1)) {
+        b = false;
+    }
+    else {
+        b = true;
+    }
+    return b;
+}
